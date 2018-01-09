e4aa255b99ba8f4f0c0d9c66fa2d198975ebb684
DavidUps <davidarribas.ciclos@gmail.com>
2018-01-09 21:37:23 +0100

    Password Worked

--- 00_Password/password.py
+++ 00_Password/password.py
@@ -1,11 +1,29 @@
-password = ''
+
+longi = False
+mayus = False
+minus = False
+num = False
 
 print("Escriba la contraseña: ")
-password = input()
-if len(password) < 8 :
-    print("Contraseña invalida")
-else:
-    print("Contraseña valida")
 
-for i in len(password):
-    print("Yeii")
+password = input()
+
+if len(password) > 8:
+    longi = True
+
+for letra in password:
+    if letra.isupper():
+        mayus = True
+
+for letra in password:
+    if letra.islower():
+        minus = True
+
+for letra in password:
+    if letra.isdigit():
+        num = True
+
+if longi and mayus and minus and num:
+    print("Contraseña corecta")
+else:
+    print("Revisa la contraseña")
